47c1ba09fd766893efb0702790f75adbc23ac542
Vishal Doshi <vishal.doshi@gmail.com>
2022-08-12 01:00:49 +0530

    Fix: tests

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const alfy = require('alfy');
+const alfy = require("alfy");
 const chrono = require("chrono-node");
 const moment = require("moment");
 
 /** @type {Date} */
 const dt = chrono.parseDate(alfy.input);
 const m = moment(dt);
-
 const ret = [];
 
-ret.push({
-	title: `[[${m.format("MMMM Do, YYYY")}]]`,
-	subtitle: "alfred",
-	arg: `[[${m.format("MMMM Do, YYYY")}]]`
-});
+if (!m.isValid()) {
+  ret.push({
+    title: "Invalid date",
+    subtitle: `Could not parse ${alfy.input}`,
+  });
+} else {
+  ret.push({
+    title: `[[${m.format("MMMM Do, YYYY")}]]`,
+    subtitle: "alfred",
+    arg: `[[${m.format("MMMM Do, YYYY")}]]`,
+  });
 
-ret.push({
-	title: m.toJSON(),
-	subtitle: "JSON (UTC)",
-	arg: m.toJSON()
-});
+  ret.push({
+    title: m.toJSON(),
+    subtitle: "JSON (UTC)",
+    arg: m.toJSON(),
+  });
 
-ret.push({
-	title: m.toISOString(true),
-	subtitle: "ISO string (with tz offset)",
-	arg: m.toISOString(true)
-});
+  ret.push({
+    title: m.toISOString(true),
+    subtitle: "ISO string (with tz offset)",
+    arg: m.toISOString(true),
+  });
 
-ret.push({
-	title: m.format("YYYY-MM-DD"),
-	subtitle: "Simple",
-	arg: m.format("YYYY-MM-DD")
-});
+  ret.push({
+    title: m.format("YYYY-MM-DD"),
+    subtitle: "Simple",
+    arg: m.format("YYYY-MM-DD"),
+  });
+}
 
 alfy.output(ret);
--- test.js
+++ test.js
@@ -1,14 +1,42 @@
-import test from 'ava';
-import alfyTest from 'alfy-test';
+import test from "ava";
+import alfyTest from "alfy-test";
 
-test(async t => {
-	const alfy = alfyTest();
-	const result = await alfy('Rainbow');
+test(async (t) => {
+  const alfy = alfyTest();
+  const result = await alfy("Rainbow");
 
-	t.deepEqual(result, [
-		{
-			title: 'Unicorn',
-			subtitle: 'Rainbow'
-		}
-	]);
+  t.deepEqual(result, [
+    {
+      title: "Invalid date",
+      subtitle: "Could not parse Rainbow",
+    },
+  ]);
+});
+
+test(async (t) => {
+  const alfy = alfyTest();
+  const result = await alfy("20 December 2020");
+
+  t.deepEqual(result, [
+    {
+      title: "[[December 20th, 2020]]",
+      subtitle: "alfred",
+      arg: "[[December 20th, 2020]]",
+    },
+    {
+      title: "2020-12-20T06:30:00.000Z",
+      subtitle: "JSON (UTC)",
+      arg: "2020-12-20T06:30:00.000Z",
+    },
+    {
+      title: "2020-12-20T12:00:00.000+05:30",
+      subtitle: "ISO string (with tz offset)",
+      arg: "2020-12-20T12:00:00.000+05:30",
+    },
+    {
+      title: "2020-12-20",
+      subtitle: "Simple",
+      arg: "2020-12-20",
+    },
+  ]);
 });
